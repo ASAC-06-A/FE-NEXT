cdb811c5c77f35d52ceb4291a2601e589efbc8bb
김정현 (Kim JungHyeon) <junghyeonkim.dev@gmail.com>
2024-12-02 14:45:22 +0900

    feat: 로드맵 페이지 Toast 적용

--- src/components/roadmap/RoadmapClientComponent.jsx
+++ src/components/roadmap/RoadmapClientComponent.jsx
@@ -10,6 +10,7 @@ import {
 } from '@/app/api/roadmapApi';
 import AddRoadmapModal from '@/components/roadmap/AddRoadmapModal';
 import CardList from '@/components/roadmap/RoadmapCard';
+import Toast from '@/lib/showToast';
 
 function RoadmapClientComponent() {
   const [roadmapDatas, setRoadmapDatas] = useState([]);
@@ -19,6 +20,7 @@ function RoadmapClientComponent() {
       const data = await getRoadmapData();
       setRoadmapDatas(data);
     } catch (error) {
+      Toast('error', '로드맵 조회를 실패하였습니다.');
       console.error('Error fetching roadmap data:', error);
     }
   };
@@ -31,9 +33,10 @@ function RoadmapClientComponent() {
   const handleAdd = async ({ roadmapTitle, desc, category }) => {
     try {
       await postRoadmapData({ roadmapTitle, desc, category });
-
       fetchRoadmapData();
+      Toast('success', '로드맵이 성공적으로 추가되었습니다.');
     } catch (error) {
+      Toast('error', '로드맵 생성을 실패하였습니다.');
       console.error('Error:', error);
     }
   };
@@ -42,9 +45,10 @@ function RoadmapClientComponent() {
   const handleDelete = async (id) => {
     try {
       await deleteRoadmapData(id);
-
       fetchRoadmapData();
+      Toast('success', '로드맵이 성공적으로 삭제되었습니다.');
     } catch (error) {
+      Toast('error', '로드맵 삭제를 실패하였습니다.');
       console.error('Error:', error);
     }
   };
@@ -53,9 +57,10 @@ function RoadmapClientComponent() {
   const handleUpdate = async (id, { roadmapTitle, desc, category }) => {
     try {
       await patchRoadmapData(id, { roadmapTitle, desc, category });
-
       fetchRoadmapData();
+      Toast('success', '로드맵이 성공적으로 수정되었습니다.');
     } catch (error) {
+      Toast('error', '로드맵 수정을 실패하였습니다.');
       console.error('Error:', error);
     }
   };
